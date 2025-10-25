ec8b56a1669553ad04b8e459030f1511a45fea62
ASD <116224668+rEHui-MbIcJIu@users.noreply.github.com>
2025-10-25 06:40:19 +0300

    Freed SftpWithParamiko class from managing paths and working with os package

--- SftpWithParamiko.py
+++ SftpWithParamiko.py
@@ -7,22 +7,16 @@ from user_model import User
 
 class SftpWithParamiko:
     def __init__(self, credentials):
-        self.user_data = []
-        self.sftp_connector = None
-
         self.credentials_dict = JsonToDict(credentials)
         self.user = User(**self.credentials_dict.data)
+
         self.transport = paramiko.Transport(self.user.hostname, self.user.port)
         self.transport.connect(username=self.user.username, password=self.user.password)
+        self.sftp_client = paramiko.SFTPClient.from_transport(self.transport)
 
     def connection(self):
-        self.sftp_connector = paramiko.SFTPClient.from_transport(self.transport)
-        return self.sftp_connector
-
-    def identify_os(self):
-        return "Windows", "Linux"
+        return self.sftp_client
 
     def proper_connection_close(self):
-        self.sftp_connector.close()
+        self.sftp_client.close()
         self.transport.close()
-
